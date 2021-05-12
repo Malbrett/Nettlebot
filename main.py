2adab41f64b306cb76d51e967342b01d4c8bfb5f
Malbrett <benhagerty1@gmail.com>
2021-05-11 22:05:18 -0700

    Implemented discord.etx commands

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import cards
+# import cards
 import discord
 import asyncio
 from discord.ext import commands
@@ -9,29 +9,31 @@ client = discord.Client()
 bot = commands.Bot(command_prefix=config.PREFIX)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
+    game = discord.Game(config.STATUS)
+    await bot.change_presence(status=discord.Status.idle, activity=game)
+    print(f'Status set to {game}')
 
 
-@client.event
-async def on_message(message):
-    message.content = message.content.lower()
-
-    if message.author == client.user:
-        return
-
-    if message.content.startswith(f'{config.PREFIX}clear'):
-        def is_mine(m):
-            return (m.author == client.user) | m.content.startswith(config.PREFIX)
-        deleted = await message.channel.purge(limit=10, check=is_mine)
-        print('Deleted {} message(s)'.format(len(deleted)))
-
-    if message.content.startswith(f'{config.PREFIX}hello'):
-        await message.channel.send('Hello!')
-
-    if message.content.startswith(f'{config.PREFIX}piss'):
-        await message.channel.send('Fuck you Arthur')
+@bot.command()
+async def hello(ctx):
+    await ctx.channel.send('Hello!')
 
 
-client.run(config.TOKEN)
+@bot.command()
+async def ping(ctx):
+    await ctx.reply("Pong!")
+
+
+@bot.command()
+async def clear(ctx):
+    def is_mine(m):
+        return (m.author == bot.user) | m.content.startswith(config.PREFIX)
+
+    deleted = await ctx.channel.purge(limit=10, check=is_mine)
+    print('Deleted {} message(s)'.format(len(deleted)))
+
+
+bot.run(config.TOKEN)
